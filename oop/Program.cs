a8b3c75879ec4a28075ebcad4f4c652714cbd95e
Mikkydice <Oibohdestiny4@gmail.com>
2022-04-03 21:46:46 +0100

    Initial commit

---
+++ oop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            int x = 5;
+            int y = 5;
+            int sum = 0;
+            sum = calc.Add(x, y);
+            Console.WriteLine(" sum of {0} + {1} = {2}", x, y, sum);
+            Console.ReadLine();
+        }
+    }
+}
